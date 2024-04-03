d55d878ebc59bf1963ce296339e874b7cd13ef7b
eeroleppalehto <eero.leppalehto@gmail.com>
2024-04-03 11:32:14 +0300

    Updated the main.go file for templ

--- cmd/main.go
+++ cmd/main.go
@@ -1,47 +1,25 @@
 package main
 
 import (
-	"html/template"
-	"io"
-
+	"github.com/eeroleppalehto/go_gallery/handler"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
-type Template struct {
-	tmpl *template.Template
-}
-
-func newTemplate() *Template {
-	return &Template{
-		tmpl: template.Must(template.ParseGlob("views/*.html")),
-	}
-}
-
-func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-	return t.tmpl.ExecuteTemplate(w, name, data)
-}
-
-type Page struct {
-	Data string
-}
-
 func main() {
 
-	e := echo.New()
+	app := echo.New()
 
-	e.Static("/static", "static")
+	app.Static("/static", "static")
 
-	e.Renderer = newTemplate()
-	e.Use(middleware.Logger())
+	app.Use(middleware.Logger())
 
-	page := Page{
-		Data: "mooi",
-	}
+	homeHandler := handler.HomeHandler{}
+	galleryHandler := handler.GalleryHandler{}
 
-	e.GET("/", func(c echo.Context) error {
-		return c.Render(200, "index", page)
-	})
+	app.GET("/", homeHandler.HandleHomeShow)
 
-	e.Logger.Fatal(e.Start(":8081"))
+	app.GET("/gallery", galleryHandler.HandleGalleryShow)
+
+	app.Logger.Fatal(app.Start(":8081"))
 }
