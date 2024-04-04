fe7c34f6f0bf0fb066f38dcad8e146a46c2222ce
eeroleppalehto <eero.leppalehto@gmail.com>
2024-04-04 15:29:59 +0300

    Added Darkmode and custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["views/**/*.templ"],
+    darkMode: "selector",
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                oswald: ["Oswald", "sans-serif"],
+                lato: ["Lato", "sans-serif"],
+            },
+        },
     },
     plugins: [],
 };
