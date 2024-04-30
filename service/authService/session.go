94414cfff5788ac1df5706a88f9ea9f6a8cf66f1
eeroleppalehto <eero.leppalehto@gmail.com>
2024-04-30 10:52:36 +0300

    Module for sessions

--- service/authService/session.go
+++ service/authService/session.go
@@ -1,35 +1,83 @@
 package authservice
 
 import (
+	"net/http"
+
+	"github.com/eeroleppalehto/go_gallery/models"
 	"github.com/gorilla/sessions"
-	"github.com/labstack/echo-contrib/session"
 	"github.com/labstack/echo/v4"
 )
 
-func CreateSession(c echo.Context) error {
-	sess, err := session.Get("session", c)
-	if err != nil {
-		return err
-	}
+var (
+	key         = []byte("super-secret-key")
+	cookieStore = "session"
+	store       = sessions.NewCookieStore(key)
+)
 
-	sess.Options = &sessions.Options{
-		Path:     "/",
-		MaxAge:   86400 * 7,
-		HttpOnly: true,
-	}
-	sess.Values["foo"] = "bar"
-	if err := sess.Save(c.Request(), c.Response()); err != nil {
-		return err
-	}
+type SessionService struct{}
 
-	return err
+type UserSession struct {
+	IsAuthenticated bool
+	Username        string
 }
 
-func ReadSession(c echo.Context) error {
-	sess, err := session.Get("session", c)
+func (s *SessionService) Init() {
+	store.Options.HttpOnly = true
+	store.Options.Secure = true
+}
+
+func (s *SessionService) Login(c echo.Context, q *models.Queries) error {
+	sess, err := store.Get(c.Request(), cookieStore)
+	if err != nil {
+		c.Response().Status = http.StatusInternalServerError
+		return err
+	}
+
+	username := c.FormValue("username")
+	password := c.FormValue("password")
+
+	user, err := q.GetUserByUsername(c.Request().Context(), username)
+	if err != nil {
+		c.Response().Status = 401
+		return err
+	}
+
+	isValidPW := ComparePassowrds([]byte(user.Password), password)
+	if !isValidPW {
+		c.Response().Status = 401
+		return err
+	}
+
+	sess.Values["authenticated"] = true
+	sess.Values["username"] = username
+	sess.Save(c.Request(), c.Response())
+	return nil
+}
+
+func (s *SessionService) Logout(c echo.Context) error {
+	sess, err := store.Get(c.Request(), cookieStore)
 	if err != nil {
 		return err
 	}
 
-	return err
+	sess.Values["authenticated"] = false
+	sess.Options.MaxAge = -1
+	sess.Save(c.Request(), c.Response())
+	return nil
+}
+
+func (s *SessionService) IsAuthenticated(c echo.Context) UserSession {
+	sess, _ := store.Get(c.Request(), cookieStore)
+
+	if auth, ok := sess.Values["authenticated"].(bool); !ok || !auth {
+		return UserSession{
+			false,
+			"",
+		}
+	}
+
+	return UserSession{
+		sess.Values["authenticated"].(bool),
+		sess.Values["username"].(string),
+	}
 }
