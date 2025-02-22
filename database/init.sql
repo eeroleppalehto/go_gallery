e79fea1444ad3444be5f0e10f1ebeda87f96d63c
eeroleppalehto <eero.leppalehto@gmail.com>
2025-02-22 20:20:29 +0200

    Update init.sql

--- database/init.sql
+++ database/init.sql
@@ -26,3 +26,25 @@ CREATE TABLE `photo` (
   KEY `fk_photo_user_idx` (`user_id`),
   CONSTRAINT `fk_photo_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+INSERT INTO user (username, email, password)
+VALUES ('admin', 'admin@gollery.com', 'admin');
+
+INSERT INTO photo (user_id, title, description, filename, date)
+VALUES 
+    (1, 'Test Photo 1', 'This is a test photo', 'image-1.jpg', NOW()),
+    (1, 'Test Photo 2', 'This is a test photo', 'image-2.jpg', NOW()),
+    (1, 'Test Photo 3', 'This is a test photo', 'image-3.jpg', NOW()),
+    (1, 'Test Photo 4', 'This is a test photo', 'image-4.jpg', NOW()),
+    (1, 'Test Photo 5', 'This is a test photo', 'image-5.jpg', NOW()),
+    (1, 'Test Photo 6', 'This is a test photo', 'image-6.jpg', NOW()),
+    (1, 'Test Photo 7', 'This is a test photo', 'image-7.jpg', NOW()),
+    (1, 'Test Photo 8', 'This is a test photo', 'image-8.jpg', NOW()),
+    (1, 'Test Photo 9', 'This is a test photo', 'image-9.jpg', NOW()),
+    (1, 'Test Photo 10', 'This is a test photo', 'image-10.jpg', NOW()),
+    (1, 'Test Photo 11', 'This is a test photo', 'image-11.jpg', NOW()),
+    (1, 'Test Photo 12', 'This is a test photo', 'image-12.jpg', NOW()),
+    (1, 'Test Photo 13', 'This is a test photo', 'image-13.jpg', NOW()),
+    (1, 'Test Photo 14', 'This is a test photo', 'image-14.jpg', NOW()),
+    (1, 'Test Photo 15', 'This is a test photo', 'image-15.jpg', NOW()),
+    (1, 'Test Photo 16', 'This is a test photo', 'image-16.jpg', NOW());
